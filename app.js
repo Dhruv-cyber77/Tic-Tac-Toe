a40d99c458493b0ffeebedf3f02c63c3c52c889d
Dhruv-cyber77 <dhruvchhimpa024@gamil.com>
2026-06-17 12:12:41 +0530

    Added responsivness for moblie

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 let turn = "X";
+let winnerfound = false;
 
+const h1 = document.querySelector("h1");
 const btns = document.querySelectorAll(".btn button");
+const board = document.querySelector(".btn");
 const start = document.querySelector(".start");
 const reset = document.querySelector(".reset");
 const players = document.querySelector(".players");
+const result = document.querySelector(".result");
 
 function startbutton() {
     btns.forEach(btn => {
@@ -31,14 +35,16 @@ let winnerpattern = [
 ];
 
 start.addEventListener("click",() =>{
-    start.disabled = true;
+
     startbutton();
 
     start.innerText = "Starting Game... "
     setTimeout(() => {
+        board.classList.remove("none");
         start.classList.add("none")
         reset.classList.remove("none")
         players.classList.remove("none")
+        h1.classList.remove("none")
     }, 1000);
 
     start.disabled = false;
@@ -53,19 +59,57 @@ btns.forEach(btn => {
             btn.innerText = turn;
             turn = "X";
         }   
+         btn.disabled = true
+         checkWinner();
     });      
 });
 
-btns.forEach(btn => {
-    btn.addEventListener("click",() => {
-        btn.disabled = true
-    })
-});
+function checkWinner() {
 
-winnerpattern.forEach(pattern => {
+    winnerfound = false;
 
-    let pos1 = btns[pattern[0]].innerText;
-    let pos2 = btns[pattern[1]].innerText;
-    let pos3 = btns[pattern[2]].innerText;
-    
-});
+    winnerpattern.forEach(pattern => {
+
+        let pos1 = btns[pattern[0]].innerText;
+        let pos2 = btns[pattern[1]].innerText;
+        let pos3 = btns[pattern[2]].innerText;
+
+        if(pos1 !== "" && pos2 !== "" && pos3 !== "") {
+            if(pos1 === pos2 && pos2 === pos3) {
+                winnerfound = true;
+
+                btns[pattern[0]].style.backgroundColor = "green";
+                btns[pattern[1]].style.backgroundColor = "green";
+                btns[pattern[2]].style.backgroundColor = "green";
+
+                result.innerText = `${pos1} Won!`;
+
+                endbutton();
+             }
+        }
+    });
+            if(!winnerfound){
+                let allfilled = true;
+
+                btns.forEach(btn => {
+                    if(btn.innerText === ""){
+                        allfilled = false;
+                    }
+                });
+
+                if(allfilled){
+                    result.innerText = "Its a Draw"
+                }
+             }
+}
+
+reset.addEventListener("click",() => {
+    btns.forEach(btn => {
+        btn.style.backgroundColor = "white";
+        btn.innerText = "";
+    });
+        turn = "X";
+        result.innerText = "Its player 1's turn";
+        winnerfound = false;
+        startbutton();
+})
